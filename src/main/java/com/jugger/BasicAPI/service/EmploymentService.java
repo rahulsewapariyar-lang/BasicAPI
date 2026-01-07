aa808e9103c2286dc6d5138247251b6148a6bf33
rahul161616 <rahulsewa1616@gmail.com>
2026-01-07 17:17:34 +0545

    chore: comments added

--- src/main/java/com/jugger/BasicAPI/service/EmploymentService.java
+++ src/main/java/com/jugger/BasicAPI/service/EmploymentService.java
@@ -17,21 +17,26 @@ public class EmploymentService {
     public EmploymentService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+    
+    //get all employees
     public List<Employee> getEmployees(){
         return employeeRepository.findAll();
     }
+    //adding multiple employees
     public List<Employee> addEmployees(List<Employee> employees){
         if (employees == null) {
             return List.of();
         }
         return employeeRepository.saveAll(employees);
     }
+    //add single employee
     public Employee add(Employee employee){
         if (employee == null){
             throw new IllegalArgumentException("Employee must not be null"); 
         }
         return employeeRepository.save(employee);
     }
+    //get employee by id
     public Employee getEmployeeById(Long id) {
         if(id == null){
             throw new IllegalArgumentException("ID must not be null");
